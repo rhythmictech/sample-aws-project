bf58c44aaebcde81dfadf26b51057df73fdcb044
Steven Black <genseb7@gmail.com>
2021-12-07 17:44:25 -0500

    optimistic versions, restore files

--- account/main.tf
+++ account/main.tf
@@ -1,6 +1,6 @@
 module "tags" {
   source  = "rhythmictech/tags/terraform"
-  version = "1.1.0"
+  version = "~> 1.1.0"
 
   names = [
     "account",
@@ -17,22 +17,32 @@ module "tags" {
 
 module "rhythmic_iam_roles" {
   source         = "rhythmictech/rhythmic-iam-roles/aws"
-  version        = "1.1.0"
+  version        = "~> 1.1.0"
   role_prefix    = var.iam_role_prefix
   master_account = var.iam_master_account
 }
 
 module "s3logging_bucket" {
-  source        = "rhythmictech/s3logging-bucket/aws"
-  version       = "1.0.1"
+  source  = "rhythmictech/s3logging-bucket/aws"
+  version = "~> 1.0.1"
+
   bucket_suffix = "account"
   region        = var.region
   tags          = module.tags.tags_no_name
+
+  # store for 1 yr
+  lifecycle_rules = [{
+    id                            = "expire"
+    enabled                       = true
+    prefix                        = null
+    expiration                    = 365
+    noncurrent_version_expiration = 365
+  }]
 }
 
 module "cloudtrail_bucket" {
   source         = "rhythmictech/cloudtrail-bucket/aws"
-  version        = "1.2.0"
+  version        = "~> 1.2.0"
   logging_bucket = module.s3logging_bucket.s3logging_bucket_name
   region         = var.region
   tags           = module.tags.tags_no_name
@@ -40,7 +50,7 @@ module "cloudtrail_bucket" {
 
 module "cloudtrail_logging" {
   source            = "rhythmictech/cloudtrail-logging/aws"
-  version           = "1.1.0"
+  version           = "~> 1.1.0"
   region            = var.region
   cloudtrail_bucket = module.cloudtrail_bucket.s3_bucket_name
   kms_key_id        = module.cloudtrail_bucket.kms_key_id
